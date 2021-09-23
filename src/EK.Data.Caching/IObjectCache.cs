3f653efeb4b98f6487282eb5ef0da7468d1a25c8
kdcllc <info@kingdavidconsulting.com>
2021-09-23 14:45:02 -0400

    adding caching object

---
+++ src/EK.Data.Caching/IObjectCache.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace EK.Data.Caching
+{
+    public interface IObjectCache
+    {
+        int GetSize();
+
+        int GetCacheCount();
+
+        List<object> GetAll();
+
+        object Read(string code);
+
+        bool Exists(string code);
+
+        object Read(int id);
+
+        void Clear();
+
+        bool Exists(int id);
+
+        bool Exists(int? id);
+
+        void Save(int? id, string code, object data);
+
+        void Save(string code, object data);
+
+        void Delete(int? id, string code);
+    }
+}
